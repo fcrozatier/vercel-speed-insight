16916e99d3f7a3d22255a6984c18e449209c91c2
fcrozatier <frederic.crozatier@protonmail.com>
2023-05-12 12:10:18 +0200

    configure web vitals

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,31 @@
+<script>
+  import { webVitals } from "$lib/vitals";
+  import { browser } from "$app/environment";
+  import { page } from "$app/stores";
+
+  let analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
+
+  console.log("analyticsId:", analyticsId);
+
+  $: if (browser && analyticsId) {
+    console.log("analyticsId:", analyticsId);
+
+    webVitals({
+      path: $page.url.pathname,
+      params: $page.params,
+      analyticsId,
+    });
+  }
+</script>
+
+<header>
+  <nav>
+    <a href="/">home</a>
+    <a href="/about">about</a>
+    <a href="/contact">contact</a>
+  </nav>
+</header>
+
+<main>
+  <slot />
+</main>
